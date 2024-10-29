9899c5577f9589e16daf3b57f626393bb30c6176
Uliana Kolodii <ulianakolodii@gmail.com>
2024-10-29 23:18:28 +0200

    feat: add flow component

---
+++ app/[id]/components/Flow.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useCallback, DragEventHandler, FC } from "react";
+import {
+  ReactFlow,
+  useNodesState,
+  useEdgesState,
+  Controls,
+  Node,
+  Edge,
+} from "@xyflow/react";
+import "@xyflow/react/dist/base.css";
+import "../../../tailwind.config";
+import HeroNode from "./HeroNode";
+import FilmNode from "./FilmNode";
+import ShipNode from "./ShipNode";
+
+const nodeTypes = {
+  heroNode: HeroNode,
+  filmNode: FilmNode,
+  shipNode: ShipNode,
+};
+
+const Flow: FC<{
+  initialNodes: Array<Node>;
+  initialEdges: Array<Edge>;
+}> = ({ initialNodes, initialEdges }) => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const handleDragOver: DragEventHandler<HTMLDivElement> = useCallback(
+    (event) => {
+      event.preventDefault();
+      event.dataTransfer.dropEffect = "move";
+    },
+    []
+  );
+
+  return (
+    <div className="w-screen h-screen">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onDragOver={handleDragOver}
+        nodeTypes={nodeTypes}
+        fitView
+        className="bg-teal-50"
+      >
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+};
+
+export default Flow;
