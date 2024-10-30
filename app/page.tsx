4dca25f4df8608e5913eab339c12b11058c5f5a1
Uliana Kolodii <ulianakolodii@gmail.com>
2024-10-30 16:52:44 +0200

    chore: add env variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@ import fetchPeople from "./api/fetchPeople";
 import HeroesTable from "./components/HeroesTable";
 
 const Home = async () => {
-  const response = await fetchPeople(
-    `https://sw-api.starnavi.io/people/?page=1`
-  );
+  const response = await fetchPeople(`${process.env.BASE_URL}/people/?page=1`);
   return <HeroesTable heroes={response.results} next={response.next} />;
 };
 
