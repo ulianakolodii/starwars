e93a5688f0a44cfe08a3a199e4ebe3c69e86eaf5
Uliana Kolodii <ulianakolodii@gmail.com>
2024-10-30 13:28:09 +0200

    feat: add test config

---
+++ jest.setup.ts
@@ -0,0 +1,22 @@
+global.ResizeObserver = class {
+  constructor(callback) {
+    this.callback = callback;
+  }
+
+  observe() {}
+
+  unobserve() {}
+
+  disconnect() {}
+};
+global.IntersectionObserver = class {
+  constructor(callback) {
+    this.callback = callback;
+  }
+
+  observe() {}
+
+  unobserve() {}
+
+  disconnect() {}
+};
